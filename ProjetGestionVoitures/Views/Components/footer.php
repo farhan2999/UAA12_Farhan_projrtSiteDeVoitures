c2353e3d3ee07bd8b487732925abf52d9f676209
Abuzour Farhan <200550@CCM-Asty-Moulin.local>
2025-03-26 14:04:46 +0100

    26/03/25

---
+++ ProjetGestionVoitures/Views/Components/footer.php
@@ -0,0 +1,16 @@
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/Assets/Css/flexible">
+    <link rel="stylesheet" href="/Assets/Css/style.css">
+    <title>BB</title>
+</head>
+<footer class="flexible justify-content-space-between">
+    <p class="p">Abuzour Farhan</p>
+    <div class="non3">
+        <a  href="https://www.instagram.com/centreastymoulin_namur/"><img id="padding" class="imageIcon non3" src="/Assets/IMG/instagram.png" alt="image instagram"></a>
+        <a  href="https://www.facebook.com/login/?next=https%3A%2F%2Fwww.facebook.com%2FCentreAstyMoulin%2F%3Flocale%3Dfr_FR"><img id="padding" class="imageIcon non3" src="/Assets/IMG/facebook.png" alt="image facebook"></a>
+    </div>
+</footer>
---
+++ ProjetGestionVoitures/Views/Components/header.php
@@ -0,0 +1,11 @@
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Css/base.css">
+    <link rel="stylesheet" href="Css/animation.css">
+    <link rel="stylesheet" href="Css/flex.css">
+    <link rel="stylesheet" href="Css/form.css">
+    <title>BB</title>
+</head>
