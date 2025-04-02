3abc762560106048ad9c4482d894aca17e84f0fe
farhan2999 <153739907+farhan2999@users.noreply.github.com>
2025-04-02 20:40:26 +0200

    02/04/25

--- ProjetGestionVoitures/Config/connectDataBase.php
+++ ProjetGestionVoitures/Config/connectDataBase.php
@@ -2,6 +2,8 @@
 
 try{
     $strConnexion = "mysql:host=10.10.51.98;dbname=farahan;port=3306" ;
+    // $strConnexion = "mysql:host=localhost;dbname=farhan;port=3306" ;
+    //$pdo=new PDO($strConnexion,"root","root",[
     $pdo=new PDO($strConnexion,"Farahan","root",[
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
